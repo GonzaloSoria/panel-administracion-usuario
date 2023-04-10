06847bfd3b721fd2bca0ca123efb26b577fb0172
GonzaloSoria <gonzalo.sooria@gmail.com>
2023-04-10 16:55:41 -0300

    first commit

---
+++ backend/src/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+/*************** config ***************/
+
+app.set('port', process.env.PORT || 4000);
+
+module.exports = app;
