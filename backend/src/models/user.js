6d24137b0e8ab000937b46cb3e04f1eec483229b
GonzaloSoria <gonzalo.sooria@gmail.com>
2023-04-10 20:23:14 -0300

    create user model

---
+++ backend/src/models/user.js
@@ -0,0 +1,13 @@
+const { Schema, model } = require('mongoose');
+
+const user_schema = new Schema({
+    name: String,
+    lastname: String,
+    age: Number,
+    phone: String,
+    email: String
+},{
+    timestamps: true
+})
+
+module.exports = model('user', user_schema);
