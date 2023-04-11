109770d5715fd8b6ca4c293bca48bf1b3ed734f2
GonzaloSoria <gonzalo.sooria@gmail.com>
2023-04-11 11:29:46 -0300

    create get, delete and update

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,7 +1,7 @@
 const User = require('../models/User');
 const user_ctrl = {};
 
-user_ctrl.getUser = async (req, res) => {
+user_ctrl.getUsers = async (req, res) => {
     const users = await User.find();
     const users_data = await users.json();
     return users_data;
@@ -10,23 +10,34 @@ user_ctrl.getUser = async (req, res) => {
 user_ctrl.createUser = async (req, res) => {
     const { name, lastname, age, phone, email } = req.body;
     const new_user = new User({
-        name: name,
-        lastname: lastname,
-        age: age,
-        phone: phone,
-        email: email
+        name,
+        lastname,
+        age,
+        phone,
+        email
     });
     await new_user.save();
 };
 
 user_ctrl.getUser = async (req, res) => {
-    
-}
+    const user = await User.findById(req.params.id);
+    const user_found = await user.json();
+    return user_found;
+};
 
-user_ctrl.getUser = async (req, res) => {
-    
-}
+user_ctrl.deleteUser = async (req, res) => {
+    await User.findByIdAndDelete(req.params.id);
+};
 
-user_ctrl.getUser = async (req, res) => {
-    
-}
+user_ctrl.updateUser = async (req, res) => {
+    const { name, lastname, age, phone, email } = req.body;
+    await User.findByIdAndUpdate(req.params.id, {
+        name,
+        lastname,
+        age,
+        phone,
+        email
+    });
+};
+
+module.exports = user_ctrl;
